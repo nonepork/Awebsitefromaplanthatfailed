3447d9bee1d370b5e310d92ce8584e1fba32fe6f
nonepork <59335048+nonepork@users.noreply.github.com>
2024-03-04 18:59:53 +0800

    Sound works now

--- public/0.js
+++ public/0.js
@@ -1,27 +1,32 @@
 window.addEventListener("DOMContentLoaded", () => {
     var star = document.getElementById("middle-cross");
     var intro = document.getElementById("intro");
+    var middleCross = document.querySelector(".middle-cross");
+    var vAndHCross1 = document.getElementsByClassName('v-and-h-cross')[0];
+    var vAndHCross2 = document.getElementsByClassName('v-and-h-cross')[1];
 
-    // TODO: add an enter button or some sort of stuff to make audio play works
-    var audio = new Audio("kaigan.mp3").play();
+    document.addEventListener("click", Clicked);
+    function Clicked() {
 
-    if (audio !== undefined) {
-        audio.then(_ => {
-            console.log("Autoplay started!");
-        }).catch(_ => {
-            console.log("Autoplay was prevented.");
+        new Audio('kaigan.mp3').play();
+        document.removeEventListener('click', Clicked);
+
+        // This works, why? I haven't the faintest
+        middleCross.classList.add("middle-cross-animation");
+        middleCross.classList.add("middle-cross-animation::after");
+        vAndHCross1.style.animation = "ig 2s forwards";
+        vAndHCross2.style.animation = "an 2s forwards";
+
+        star.addEventListener("animationend", function() {
+            setTimeout(() => {
+                intro.style.animation = "descend 2s ease-in-out forwards";
+
+                intro.addEventListener("animationend", function() {
+                    intro.style.display = "none"; // Hide the intro
+                });
+            }, 1000);
         });
     }
-
-    star.addEventListener("animationend", function() {
-        setTimeout(() => {
-            intro.style.animation = "descend 2s ease-in-out forwards";
-
-            intro.addEventListener("animationend", function() {
-                intro.style.display = "none"; // Hide the intro
-            });
-        }, 1000);
-    });
 
     const textContent = document.getElementById("decide")
     const form = document.getElementById('form');
